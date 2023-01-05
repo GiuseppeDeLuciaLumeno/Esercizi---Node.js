2134f4a26aa9a83168543657454f305f3e2ef5f0
Giupetrucci <giupetrucci@libero.it>
2023-01-05 20:23:31 +0100

    ex

--- ex.js
+++ ex.js
@@ -1,33 +1,25 @@
-const players = ["Tina", "Jorge", "Julien", "Giuseppe"]
+const players = ["Joe", "Caroline", "Sabrina"]
 
 function luckyDraw(player) {
-    return new Promise((resolve, reject) => {
-      const win = Boolean(Math.round(Math.random()));
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
 
-      process.nextTick(() => {
-        if (win) {
-          resolve(`${player} won a prize in the draw!`);
-        } else {
-          reject(new Error(`${player} lost the draw.`));
-        }
-      });
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
     });
-  }
-
+  });
+}
 
 
 players.forEach(player => {
-   const getResults = async() => {
+  const result = luckyDraw(player)
 
-    try {
-        const result = await luckyDraw(player)
-        console.log(result)
-
-    } catch (error) {
-        console.log(error);
-    }
-    }
-  return getResults()
+  .then(player => console.log(player))
+  .catch(e => console.log(e))
 })
 
 
